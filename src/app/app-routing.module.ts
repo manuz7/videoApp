e25a2506b9b1bda43363dba5f2132ecc8578ddd3
Manu <99971591+nmva227@users.noreply.github.com>
2022-09-08 08:57:45 -0300

    Homem-Aranha

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'homem-aranha',
+    loadChildren: () => import('./homem-aranha/homem-aranha.module').then( m => m.HomemAranhaPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
